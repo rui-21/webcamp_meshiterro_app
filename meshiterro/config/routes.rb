fc403ffbb14d23f795e4dcb83cd8c1b64f3176bc
rui-21 <reisu6235@gmail.com>
2021-04-17 08:50:55 +0000

    [Start]4-4

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   
 end
